698736056992f4bc2a480ddc3acc843a7e46c85d
Vineet Pandey <vinitcodez@gmail.com>
2024-09-09 09:10:42 +0530

    Add files via upload

---
+++ js-hindi/variable.js
@@ -0,0 +1,14 @@
+// variables in js
+const x = "vinit pandey"
+let y = 35
+var isEmployeed = false //var variavle have no scope
+if (x== "vinit pandey") {
+    var income = 12000
+    isEmployeed = true
+    console.log(isEmployeed)
+}
+income = 14000;
+console.log (x)
+console.log(y)
+console.log(isEmployeed)
+console.log(income)
